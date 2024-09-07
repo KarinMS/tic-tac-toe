6d2245cb141663812cb1542985865d9c56696347
KarinMS <carin.maxim@yandex.ru>
2024-09-07 18:49:29 +0300

    Добавлена функция позволяющая делать ход ask_and_make_move

--- tic-tac-toe.py
+++ tic-tac-toe.py
@@ -1,5 +1,5 @@
 #Функция, которая рисует игровое поле 3х3
-def drawing_board(board):
+def draw_board(board):
     #Запускаем цикл, который проходит по всем 3 строкам доски
     for i in range(3):
         #Метод join для объединения в 1 строку
@@ -7,7 +7,21 @@ def drawing_board(board):
         print('---------')
 #Тест функции
 board = [[' ' for i in range(3)] for j in range(3)]
-drawing_board(board)
+player = "X"
+draw_board(board)
+
 
 #Функция, которая позволяет делать ход
 def ask_and_make_move(player, board):
+    # Дать игроку возможность сделать ход
+    x, y = input(f"{player}, введите координаты x и y (например: 1 1): ").split().strip()
+    # Преобразовать координаты в целые числа
+    x, y = int(x), int(y)
+    # находится ли координата в пределах поля и свободно ли место
+    if (0 < x < 2) and (0 < y < 2) and (board[x, y] == " "):
+        # если свободно, записать значение игрока (Х или 0) в ячейку
+        board[x, y] = player
+    else:
+        print("Это место уже занято. Попробуйте снова.")
+        ask_and_make_move(player, board)
+
