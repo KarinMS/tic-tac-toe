8c3912d0e477d48d818a6ea923029b28745fbe49
KarinMS <carin.maxim@yandex.ru>
2024-09-07 19:31:34 +0300

    Функция ask_and_make_move разделена на две функции ask_move и make_move

--- tic-tac-toe.py
+++ tic-tac-toe.py
@@ -14,14 +14,25 @@ draw_board(board)
 #Функция, которая позволяет делать ход
 def ask_and_make_move(player, board):
     # Дать игроку возможность сделать ход
-    x, y = input(f"{player}, введите координаты x и y (например: 1 1): ").split().strip()
+    x, y = ask_move(player, board)
+    make_move(player, board, x, y)
+
+#Функция, которая позволяет пользователю ввести координаты хода
+def ask_move(player, board):
+    # Дать игроку возможность сделать ход
+    x, y = input(f"{player}, введите координаты x и y (например: 1 1): ").strip().split()
     # Преобразовать координаты в целые числа
     x, y = int(x), int(y)
     # находится ли координата в пределах поля и свободно ли место
-    if (0 < x < 2) and (0 < y < 2) and (board[x, y] == " "):
-        # если свободно, записать значение игрока (Х или 0) в ячейку
-        board[x, y] = player
+    if (0 <= x <= 2) and (0 <= y <= 2) and (board[x][y] == " "):
+        # если свободно, вернуть ее координаты
+        return x, y
     else:
-        print("Это место уже занято. Попробуйте снова.")
-        ask_and_make_move(player, board)
+        print("Клетка занята. Попробуйте снова.")
+        return ask_move(player, board)
 
+#Функция, которая непосредственно делает ход
+def make_move(player, board, x, y):
+    board[x][y] = player
+
+ask_and_make_move(player, board)
